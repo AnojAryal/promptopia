831d75440fccca0ed57edacd5e65c3d5b2798259
AnojAryal <anoj1810@gmail.com>
2025-02-13 14:27:19 +0545

    setting up providers with client_id and client_secret

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,24 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+console.log({
+  GOOGLE_ID: process.env.GOOGLE_ID,
+  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+})
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  async session(session) {
+
+  },
+  async signIn( profile) {
+
+  }
+});
+
+export {handler as GET, handler as POST};
