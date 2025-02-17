8377de3c1fbfc5909143be106abab303238f85a4
AnojAryal <anoj1810@gmail.com>
2025-02-17 14:04:54 +0545

    create prompt hook

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { useSession } from "/next-auth/react";
+import { useSession } from "next-auth/react";
 import { useReducer } from "next/navigation";
 
 import Form from "@components/Form";
@@ -13,7 +13,29 @@ const CreatePrompt = () => {
     tag: "",
   });
 
-  const CreatePrompt = async (e) => {};
+  const CreatePrompt = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
+
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
+
+      if (response.ok) {
+        Router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
 
   return (
     <Form
@@ -21,7 +43,7 @@ const CreatePrompt = () => {
       post={post}
       setPost={setPost}
       submitting={submitting}
-      handleSubmit={createPrompt}
+      handleSubmit={CreatePrompt}
     />
   );
 };
