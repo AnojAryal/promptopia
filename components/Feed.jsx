4637c8432bb7147aa5d497ee2e0d6aa826e7d111
AnojAryal <anoj1810@gmail.com>
2025-02-18 14:52:29 +0545

    building feed

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,6 +1,41 @@
-const Feed = () => {
+'use client'
+
+import {useState, useEffect} from 'react';
+
+import PromptCard from './PromptCard';
+
+const PromptCradLIst = ({data, handleTagClick}) => {
   return (
-    <div>Feed</div>
+    <div className='mt-16 prompt_layout'>
+      
+    </div>
+  )
+}
+
+const Feed = () => {
+  const [searchText, setSearchText] = useState('');
+
+  const handleSearchChange = (e) => {
+
+  }
+
+  return (
+    <section className='feed'>
+      <form className='relative w-full flex-center'>
+      <input
+      type='text'
+      placeholder='Search For a tag or a username'
+      value={searchText}
+      onChange={handleSearchChange}
+      required
+      className='search_input peer'
+      />
+      </form>
+      <PromptCardList
+      data={[]}
+      handleTagClick = {()=> {}}
+      />
+    </section>
   )
 }
 
