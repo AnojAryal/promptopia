f5202c9b9152a162fa2480f104459b6ae949f625
AnojAryal <anoj1810@gmail.com>
2025-02-22 17:25:20 +0545

    delete by id

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -48,3 +48,17 @@ export const PATCH = async (request, { params }) => {
     return new Response("Error Updating Prompt", { status: 500 });
   }
 };
+
+//delete
+export const DELETE = async (request, { params }) => {
+  try {
+    await ConnectToDB();
+
+    // Find the prompt by ID and remove it
+    await Prompt.findByIdAndRemove(params.id);
+
+    return new Response("Prompt deleted successfully", { status: 200 });
+  } catch (error) {
+    return new Response("Error deleting prompt", { status: 500 });
+  }
+};
