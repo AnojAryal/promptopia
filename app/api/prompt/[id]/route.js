65d9eca5f49504f8d9fdd978cc0f2ac507f1e14f
AnojAryal <anoj1810@gmail.com>
2025-02-22 17:23:45 +0545

    patch by id

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -22,3 +22,29 @@ export const GET = async (request, { params }) => {
     });
   }
 };
+
+//patch
+export const PATCH = async (request, { params }) => {
+  const { prompt, tag } = await request.json();
+
+  try {
+    await ConnectToDB();
+
+    // Find the existing prompt by ID
+    const existingPrompt = await Prompt.findById(params.id);
+
+    if (!existingPrompt) {
+      return new Response("Prompt not found", { status: 404 });
+    }
+
+    // Update the prompt with new data
+    existingPrompt.prompt = prompt;
+    existingPrompt.tag = tag;
+
+    await existingPrompt.save();
+
+    return new Response("Successfully updated the Prompts", { status: 200 });
+  } catch (error) {
+    return new Response("Error Updating Prompt", { status: 500 });
+  }
+};
